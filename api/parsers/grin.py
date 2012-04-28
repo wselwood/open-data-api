16f67dab0a00442a643d09d3be885ad834ce61a0
William Selwood <wselwood@gmail.com>
2012-04-28 15:52:09 +0100

    Small fixes to the grin parser. Subjects cleaned up, no empty entries and each is now trimmed. Description and short description are now trimmed. Made each image size optional. There is an entry in JPL with no medium image. It  should now deal with any other sizes being missing.

--- api/parsers/grin.py
+++ api/parsers/grin.py
@@ -122,7 +122,9 @@ def find_all(string, occurrence):
 
 
 def get_pages():
-	center_list = ['AMES','DFRC','GRC','GSFC','HQ','JPL','JSC','KSC','LARC','MSFC','SSC']
+	# replaced to make add quicker and not to spam nasa
+	#center_list = ['AMES','DFRC','GRC','GSFC','HQ','JPL','JSC','KSC','LARC','MSFC','SSC']
+	center_list = ['JPL']
 	for center in center_list:
 		html = lxml.html.parse('http://grin.hq.nasa.gov/BROWSE/' + center + '.html')
 		page = lxml.html.tostring(html, pretty_print=True, method="html")
@@ -137,6 +139,10 @@ def get_pages():
 			count = count + 1
 		
 	return ''
+
+def clean_string_list(stringList):
+	for entry in stringList:
+		yield entry.strip('\n\r ,\t')
 	
 def get_a_page(url):
 	html = ""
@@ -163,11 +169,11 @@ def get_a_page(url):
 
 	startPos = page.find('<!-- ONE-LINE-DESCRIPTION-BEGIN -->')
 	endPos = page.find('<!-- ONE-LINE-DESCRIPTION-END -->')
-	shortDescription = page[startPos+35:endPos]
+	shortDescription = page[startPos+35:endPos].strip()
 
 	startPos = page.find('<!-- DESCRIPTION-BEGIN -->')
 	endPos = page.find('<!-- DESCRIPTION-END -->')
-	description = page[startPos+27:endPos]
+	description = page[startPos+27:endPos].strip()
 
 	startPos = page.find('<!-- KEYWORD-BEGIN -->')
 	endPos = page.find('<!-- KEYWORD-END -->')
@@ -176,9 +182,8 @@ def get_a_page(url):
 
 	startPos = page.find('<!-- SUBJECT-BEGIN -->')
 	endPos = page.find('<!-- SUBJECT-END -->')
-	subjects = page[startPos+22:endPos]
-	subjects.strip()
-	subjectList = subjects.split(',')
+	subjects = page[startPos+22:endPos].strip().strip(',')
+	subjectList = list(clean_string_list(subjects.split(',')))
 
 	startPos = page.find('<!-- CENTER-BEGIN -->')
 	endPos = page.find('<!-- CENTER-END -->')
@@ -203,131 +208,157 @@ def get_a_page(url):
 	origSource = origSource.strip()
 
 	startPos = page.find('<td id="r2" headers="c1"><a href="')
-	remaining = page[startPos+34:startPos+200]
-	endPos = remaining.find('">')
-	thumbnailUrl = remaining[0:endPos]
-
-	startPos = page.find('headers="c2 r2"')
-	remaining = page[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	thumbnailType = remaining[0:endPos]
-	thumbnailType = thumbnailType.strip()
-
-	startPos = page.find('headers="c3 r2"')
-	remaining = page[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	thumbnailWidth = remaining[0:endPos]
-	thumbnailWidth = thumbnailWidth.strip()
-
-	startPos = page.find('headers="c4 r2')
-	remaining = page[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	thumbnailHeight = remaining[0:endPos]
-	thumbnailHeight = thumbnailHeight.strip()
-
-	startPos = page.find('headers="c5 r2"')
-	remaining = page[startPos+30:startPos+60]
-	endPos = remaining.find('</td>')
-	thumbnailSize = remaining[0:endPos]
-	thumbnailSize = thumbnailSize.strip()
-
+	if startPos >= 0 :
+		remaining = page[startPos+34:startPos+200]
+		endPos = remaining.find('">')
+		thumbnailUrl = remaining[0:endPos]
+	
+		startPos = page.find('headers="c2 r2"')
+		remaining = page[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		thumbnailType = remaining[0:endPos]
+		thumbnailType = thumbnailType.strip()
+	
+		startPos = page.find('headers="c3 r2"')
+		remaining = page[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		thumbnailWidth = remaining[0:endPos]
+		thumbnailWidth = thumbnailWidth.strip()
+	
+		startPos = page.find('headers="c4 r2')
+		remaining = page[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		thumbnailHeight = remaining[0:endPos]
+		thumbnailHeight = thumbnailHeight.strip()
+	
+		startPos = page.find('headers="c5 r2"')
+		remaining = page[startPos+30:startPos+60]
+		endPos = remaining.find('</td>')
+		thumbnailSize = remaining[0:endPos]
+		thumbnailSize = thumbnailSize.strip()
+	else :
+		thumbnailUrl = ''
+		thumbnailType = 'None'
+		thumbnailWidth = '0'
+		thumbnailHeight = '0'
+		thumbnailSize = '0'
 
 
 	startPos = page.find('<td id="r3" headers="c1"><a href="')
-	remaining = page[startPos+34:startPos+200]
-	endPos = remaining.find('">')
-	smallUrl = remaining[0:endPos]
-
-	startPos = page.find('headers="c2 r3"')
-	remaining = page[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	smallType = remaining[0:endPos]
-	smallType = smallType.strip()
-
-	startPos = page.find('headers="c3 r3"')
-	remaining = page[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')	
-	smallWidth = remaining[0:endPos]
-	smallWidth = smallWidth.strip()
-
-	startPos = page.find('headers="c4 r3"')
-	remaining = page[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	smallHeight = remaining[0:endPos]
-	smallHeight = smallHeight.strip()
-
-	startPos = page.find('headers="c5 r3"')
-	remaining = page[startPos+30:startPos+60]
-	endPos = remaining.find('</td>')
-	smallSize = remaining[0:endPos]
-	smallSize = smallSize.strip()
+	if startPos >= 0 :
+		remaining = page[startPos+34:startPos+200]
+		endPos = remaining.find('">')
+		smallUrl = remaining[0:endPos]
+	
+		startPos = page.find('headers="c2 r3"')
+		remaining = page[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		smallType = remaining[0:endPos]
+		smallType = smallType.strip()
+	
+		startPos = page.find('headers="c3 r3"')
+		remaining = page[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')	
+		smallWidth = remaining[0:endPos]
+		smallWidth = smallWidth.strip()
+	
+		startPos = page.find('headers="c4 r3"')
+		remaining = page[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		smallHeight = remaining[0:endPos]
+		smallHeight = smallHeight.strip()
+	
+		startPos = page.find('headers="c5 r3"')
+		remaining = page[startPos+30:startPos+60]
+		endPos = remaining.find('</td>')
+		smallSize = remaining[0:endPos]
+		smallSize = smallSize.strip()
+	else :
+		smallUrl = ''
+		smallType = 'None'
+		smallWidth = '0'
+		smallHeight = '0'
+		smallSize = '0'
 
 
 	startPos = page.find('<td headers="c1" id="r4"><a href="')
-	remaining = page[startPos+34:startPos+200]
-	restOfRow3 = page[startPos+31:startPos+5000]
-	endPos = remaining.find('">')
-	mediumUrl = remaining[0:endPos]
-
-
-	startPos = restOfRow3.find('headers="c2 r3"')
-	remaining = restOfRow3[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	mediumType = remaining[0:endPos]
-	mediumType = mediumType.strip()
-
-
-	startPos = restOfRow3.find('headers="c3 r3"')
-	remaining = restOfRow3[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	mediumWidth = remaining[0:endPos]
-	mediumWidth = mediumWidth.strip()
-
-	startPos = restOfRow3.find('headers="c4 r3"')
-	remaining = restOfRow3[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	mediumHeight = remaining[0:endPos]
-	mediumHeight = mediumHeight.strip()
-
-	startPos = restOfRow3.find('headers="c5 r3"')
-	remaining = restOfRow3[startPos+30:startPos+60]
-	endPos = remaining.find('</td>')
-	mediumSize = remaining[0:endPos]
-	mediumSize = mediumSize.strip()
-
+	if startPos >= 0 :
+		remaining = page[startPos+34:startPos+200]
+		restOfRow3 = page[startPos+31:startPos+5000]
+		endPos = remaining.find('">')
+		
+		mediumUrl = remaining[0:endPos]
+	
+	
+		startPos = restOfRow3.find('headers="c2 r3"')
+		remaining = restOfRow3[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		mediumType = remaining[0:endPos]
+		mediumType = mediumType.strip()
+	
+	
+		startPos = restOfRow3.find('headers="c3 r3"')
+		remaining = restOfRow3[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		mediumWidth = remaining[0:endPos]
+		mediumWidth = mediumWidth.strip()
+	
+		startPos = restOfRow3.find('headers="c4 r3"')
+		remaining = restOfRow3[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		mediumHeight = remaining[0:endPos]
+		mediumHeight = mediumHeight.strip()
+	
+		startPos = restOfRow3.find('headers="c5 r3"')
+		remaining = restOfRow3[startPos+30:startPos+60]
+		endPos = remaining.find('</td>')
+		mediumSize = remaining[0:endPos]
+		mediumSize = mediumSize.strip()
+	else :
+		mediumUrl = ''
+		mediumType = 'None'
+		mediumWidth = '0'
+		mediumHeight = '0'
+		mediumSize = '0'
 
 	startPos = page.find('<td id="r5" headers="c1"><a href="')
-	remaining = page[startPos+34:startPos+200]
-	restOfRow4 = page[startPos+31:startPos+5000]
-	endPos = remaining.find('">')
-	largeUrl = remaining[0:endPos]
-
-
-	startPos = restOfRow4.find('headers="c2 r3"')
-	remaining = restOfRow4[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	largeType = remaining[0:endPos]
-	largeType = largeType.strip()
-
-	startPos = restOfRow4.find('headers="c3 r3"')
-	remaining = restOfRow4[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	largeWidth = remaining[0:endPos]
-	largeWidth = largeWidth.strip()
-
-	startPos = restOfRow4.find('headers="c4 r3"')
-	remaining = restOfRow4[startPos+31:startPos+60]
-	endPos = remaining.find('</td>')
-	largeHeight = remaining[0:endPos]
-	largeHeight = largeHeight.strip()
+	if startPos >= 0 :
+		remaining = page[startPos+34:startPos+200]
+		restOfRow4 = page[startPos+31:startPos+5000]
+		endPos = remaining.find('">')
+		largeUrl = remaining[0:endPos]
 	
-
-	startPos = restOfRow4.find('headers="c5 r3"')
-	remaining = restOfRow4[startPos+30:startPos+60]
-	endPos = remaining.find('</td>')
-	largeSize = remaining[0:endPos]
-	largeSize = largeSize.strip()
-
+	
+		startPos = restOfRow4.find('headers="c2 r3"')
+		remaining = restOfRow4[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		largeType = remaining[0:endPos]
+		largeType = largeType.strip()
+	
+		startPos = restOfRow4.find('headers="c3 r3"')
+		remaining = restOfRow4[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		largeWidth = remaining[0:endPos]
+		largeWidth = largeWidth.strip()
+	
+		startPos = restOfRow4.find('headers="c4 r3"')
+		remaining = restOfRow4[startPos+31:startPos+60]
+		endPos = remaining.find('</td>')
+		largeHeight = remaining[0:endPos]
+		largeHeight = largeHeight.strip()
+		
+	
+		startPos = restOfRow4.find('headers="c5 r3"')
+		remaining = restOfRow4[startPos+30:startPos+60]
+		endPos = remaining.find('</td>')
+		largeSize = remaining[0:endPos]
+		largeSize = largeSize.strip()
+	else :
+		largeUrl = ''
+		largeType = 'None'
+		largeWidth = '0'
+		largeHeight = '0'
+		largeSize = '0'
 
 	dataset = Grin(data_url = url, center_name = centerName, image_reference = imageRef, date_stamp = dateTimestamp, short_description = shortDescription, full_description = description, keyword_list = keywordList, subject_list = subjectList, center_code = centerCode, grin_id = grinID, image_creator = creator, original_source = origSource, thumbnail_url = thumbnailUrl, thumbnail_type = thumbnailType, thumbnail_width = thumbnailWidth, thumbnail_height = thumbnailHeight, thumbnail_size = thumbnailSize, small_url = smallUrl, small_type = smallType, small_width = smallWidth, small_height = smallHeight, small_size = smallSize, medium_url = mediumUrl, medium_type = mediumType, medium_width = mediumWidth, medium_height = mediumHeight, medium_size = mediumSize, large_url = largeUrl, large_type = largeType, large_width = largeWidth, large_height = largeHeight, large_size = largeSize)
 	dataset.save();
